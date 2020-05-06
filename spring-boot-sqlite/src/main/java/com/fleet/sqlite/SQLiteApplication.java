420e715c1a0a1e4d9f73e0e1d5cb2d6b0d688e5c
AprilHan1992 <785579683@qq.com>
2020-05-06 09:44:31 +0800

    sqlite数据库 项目示例

---
+++ spring-boot-sqlite/src/main/java/com/fleet/sqlite/SQLiteApplication.java
@@ -0,0 +1,13 @@
+package com.fleet.sqlite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SQLiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SQLiteApplication.class, args);
+	}
+
+}
