056f5906fd3e7317701b8e10cd58cee9e836ce66
AprilHan1992 <785579683@qq.com>
2020-05-07 17:17:21 +0800

    Feign 实现声明式 REST 调用 项目示例

---
+++ spring-boot-feign/src/main/java/com/fleet/feign/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.fleet.feign.controller;
+
+import com.fleet.feign.feign.ProviderFeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class HelloController {
+
+    @Resource
+    ProviderFeignClient providerFeignClient;
+
+    @RequestMapping("hello")
+    public String get() {
+        return providerFeignClient.hello();
+    }
+}
