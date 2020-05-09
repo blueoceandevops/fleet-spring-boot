11a07703d2c86eaebd1c471aa5679b7172499e47
AprilHan1992 <785579683@qq.com>
2020-05-09 10:07:22 +0800

    Security 权限管理 项目示例

--- spring-boot-security/src/main/java/com/fleet/security/config/adapter/WebSecurityConfigurer.java
+++ spring-boot-security/src/main/java/com/fleet/security/config/adapter/WebSecurityConfigurer.java
@@ -7,16 +7,16 @@ import com.fleet.security.config.handler.LogoutSuccess;
 import com.fleet.security.service.impl.UserDetailsServiceImpl;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.annotation.Resource;
 
 @Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
 public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
 
     @Resource
@@ -40,28 +40,34 @@ public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    public void configure(WebSecurity web) throws Exception {
+    public void configure(WebSecurity web) {
         web.ignoring().antMatchers("/guest/**", "/notIn", "/in", "/out", "/outError", "/unauth");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http
                 .authorizeRequests()
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .antMatchers("/user/**").hasRole("USER")
                 .antMatchers("/user/get").hasAuthority("USER:GET")
                 .anyRequest().authenticated()
-                .and().formLogin()
+                .and()
+                .formLogin()
                 .loginPage("/notIn")
                 .loginProcessingUrl("/login").permitAll()
                 .usernameParameter("name").passwordParameter("password")
                 .successHandler(authenticationSuccess).permitAll()
                 .failureHandler(authenticationFailure).permitAll()
-                .and().logout()
+                .and()
+                .logout()
                 .logoutUrl("/logout")
                 .logoutSuccessHandler(logoutSuccess).permitAll()
-                .and().exceptionHandling().accessDeniedHandler(accessDenied)
-                .and().httpBasic();
+                .and()
+                .exceptionHandling().accessDeniedHandler(accessDenied)
+                .and()
+                .httpBasic()
+                .and()
+                .csrf().disable();
     }
 }
