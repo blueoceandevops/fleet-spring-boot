9e8a07bb9d4fee088a7cb5f5dbf01e9c199516c7
AprilHan1992 <785579683@qq.com>
2020-05-09 11:59:46 +0800

    Security 权限管理 项目示例

--- spring-boot-security/src/main/java/com/fleet/security/config/adapter/WebSecurityConfigurer.java
+++ spring-boot-security/src/main/java/com/fleet/security/config/adapter/WebSecurityConfigurer.java
@@ -5,13 +5,16 @@ import com.fleet.security.config.handler.AuthenticationFailure;
 import com.fleet.security.config.handler.AuthenticationSuccess;
 import com.fleet.security.config.handler.LogoutSuccess;
 import com.fleet.security.service.impl.UserDetailsServiceImpl;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.annotation.Resource;
 
@@ -68,5 +71,15 @@ public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
                 .httpBasic()
                 .and()
                 .csrf().disable();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
