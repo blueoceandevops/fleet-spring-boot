a6ac9473b99264745c2e88cc21ad3ca067551d20
AprilHan1992 <785579683@qq.com>
2020-05-09 09:01:38 +0800

    Hystrix Dashboard 监控 项目示例

--- spring-boot-hystrix-dashboard/src/main/java/com/fleet/hystrix/dashboard/HystrixDashboardApplication.java
+++ spring-boot-hystrix-dashboard/src/main/java/com/fleet/hystrix/dashboard/HystrixDashboardApplication.java
@@ -3,11 +3,13 @@ package com.fleet.hystrix.dashboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
-@EnableCircuitBreaker
 @EnableHystrixDashboard
+@EnableCircuitBreaker
+@EnableHystrix
 public class HystrixDashboardApplication {
 
     public static void main(String[] args) {
