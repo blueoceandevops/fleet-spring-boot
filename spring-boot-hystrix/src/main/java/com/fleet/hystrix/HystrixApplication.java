e30612bd9c4a719fbfce7b5937f48fc3fe3fad7e
AprilHan1992 <785579683@qq.com>
2020-05-09 08:56:23 +0800

    Hystrix 容错处理 项目示例

--- spring-boot-hystrix/src/main/java/com/fleet/hystrix/HystrixApplication.java
+++ spring-boot-hystrix/src/main/java/com/fleet/hystrix/HystrixApplication.java
@@ -3,17 +3,15 @@ package com.fleet.hystrix;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableCircuitBreaker
 @EnableHystrix
 public class HystrixApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HystrixApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixApplication.class, args);
+    }
 
 }
