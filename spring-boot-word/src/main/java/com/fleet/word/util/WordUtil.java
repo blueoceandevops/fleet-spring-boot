3e2de9eaddf7736fef33f2e05d51dfe35e9c03e7
AprilHan1992 <785579683@qq.com>
2020-05-15 08:52:55 +0800

    Word 操作 项目示例

---
+++ spring-boot-word/src/main/java/com/fleet/word/util/WordUtil.java
@@ -0,0 +1,62 @@
+package com.fleet.word.util;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * Word工具类
+ */
+public class WordUtil {
+
+    private Configuration config;
+
+    public WordUtil() {
+        config = new Configuration(Configuration.VERSION_2_3_29);
+        config.setEncoding(Locale.getDefault(), "UTF-8");
+        config.setClassicCompatible(true);
+    }
+
+    /**
+     * 根据word xml模板生成word文档
+     */
+    public void export(Map<String, Object> map, String tempPath, String tempName, OutputStream out) throws Exception {
+        File file = getTempFile(map, tempPath, tempName);
+
+        InputStream in = new FileInputStream(file);
+        byte[] b = new byte[1024];
+        int len;
+        while ((len = in.read(b)) > 0) {
+            out.write(b, 0, len);
+        }
+        out.flush();
+        out.close();
+        in.close();
+    }
+
+    /**
+     * 生成word文档缓存文件
+     *
+     * @param map      替换数据
+     * @param tempPath 缓存目录
+     * @param tempFile resources/template下模板文件
+     * @return
+     * @throws IOException
+     * @throws TemplateException
+     */
+    public File getTempFile(Map<String, Object> map, String tempPath, String tempFile) throws Exception {
+        config.setClassForTemplateLoading(WordUtil.class, "/template");
+        Template template = config.getTemplate(tempFile);
+        File file = new File(tempPath + "temp.doc");
+        Writer writer = new OutputStreamWriter(new FileOutputStream(file), StandardCharsets.UTF_8);
+        template.process(map, writer);
+        writer.flush();
+        writer.close();
+        return file;
+    }
+}
