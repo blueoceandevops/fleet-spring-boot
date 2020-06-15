9f50cf5d0c5beafdf7e84fb4f165a8a1d7bbda3d
AprilHan1992 <785579683@qq.com>
2020-06-15 10:58:34 +0800

    本地缓存

---
+++ spring-boot-cache-caffeine/src/test/java/com/fleet/cache/caffeine/CacheCaffeineApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fleet.cache.caffeine;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CacheCaffeineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
